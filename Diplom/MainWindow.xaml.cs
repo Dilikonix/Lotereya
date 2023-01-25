8712cf3abf27ea7d8e766ae101f0891a086e984d
hones <hones@DESKTOP-EOHPKLK>
2023-01-25 21:59:10 +0300

    Добавьте файлы проекта.

---
+++ Diplom/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Diplom
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void PassTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LoginTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void forgotButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ForgotWindow window = new Windows.ForgotWindow();
+            this.Close();
+            window.ShowDialog();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.WorkWindow window = new Windows.WorkWindow();
+            this.Close();
+            window.ShowDialog();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Windows.RegisterWindow window = new Windows.RegisterWindow();
+            this.Close();
+            window.ShowDialog();
+            
+
+        }
+    }
+}
---
+++ Diplom/Windows/ForgotWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Diplom.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ForgotWindow.xaml
+    /// </summary>
+    public partial class ForgotWindow : Window
+    {
+        public ForgotWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonRecoverUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBackLogin1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            this.Close();
+            window.ShowDialog();
+        }
+    }
+}
